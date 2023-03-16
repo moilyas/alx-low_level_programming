e2d67322f8dd93c3f04f552ce857087be5d0f16a
mohamed ilyas karouata <ikarouata08@gmail.com>
2023-03-16 06:47:33 -0700

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main- A program that print with puts function
+* Return: value 0 (Success)
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
