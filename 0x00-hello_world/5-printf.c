2ed2ab5b4f06432dcb8a652e15dce3fb6f3fdc24
mohamed ilyas karouata <ikarouata08@gmail.com>
2023-03-16 06:49:27 -0700

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - A program that print with printf function
+* Return: value 0 (Success)
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
