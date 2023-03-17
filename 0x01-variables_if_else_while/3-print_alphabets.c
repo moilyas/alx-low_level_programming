feb2ed6867292c8edb20f38c2044740106bddf7d
mohamed ilyas karouata <ikarouata08@gmail.com>
2023-03-17 07:11:21 -0700

    3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - A program
+ * Return: value 0 (Success)
+ */
+int main(void)
+{
+char c = 'a';
+char d = 'A';
+ 
+while (c <= 'z')
+{
+putchar(c);
+c++;
+}
+while (d <= 'Z')
+{
+putchar(d);
+d++;
+}
+putchar('\n');
+return (0);
+}
