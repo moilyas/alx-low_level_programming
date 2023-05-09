9ff4e5de009ee4b281e04be004c9a95a1bf18a74
mohamed ilyas karouata <ikarouata08@gmail.com>
2023-05-09 13:04:35 -0700

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,7 +25,7 @@ int create_file(const char *filename, char *text_content)
 		text_content = "";
 
 	for (nletters = 0)
-		text_content[nletters]; nletters++;
+		text_content[nletters] nletters++;
 
 	rwr = write(fd, text_content, nletters);
 
